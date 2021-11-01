94a572f7cfe48afd0531ea708fae06002fc4042c
Vigi73 <vigi73@mail.ru>
2021-11-01 16:32:01 +0700

    upd11111

--- phone/obj/Release/net5.0-windows/phone.AssemblyInfo.cs
+++ phone/obj/Release/net5.0-windows/phone.AssemblyInfo.cs
@@ -11,13 +11,15 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("phone")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("УФК по Алтайскому краю")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("phone")]
+[assembly: System.Reflection.AssemblyCopyrightAttribute("Егиоя Е.Н.")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("2.0.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.0.0")]
+[assembly: System.Reflection.AssemblyProductAttribute("Справочник ВТС")]
 [assembly: System.Reflection.AssemblyTitleAttribute("phone")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("2.0.0.0")]
+[assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/Vigi73/vts__c-")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
 
