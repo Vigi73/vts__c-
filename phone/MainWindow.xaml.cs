6d60e0ce33dd3b9cbdfbe8de006d9c76c3c8b72e
Vigi73 <vigi73@mail.ru>
2021-11-01 14:14:06 +0700

    first commit

---
+++ phone/MainWindow.xaml.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SQLite;
+using System.Data;
+using System.Text.RegularExpressions;
+using System.Globalization;
+
+
+namespace phone
+
+{
+    public class MyTable
+    {
+                
+        public string ID { get; set; }
+        public string STRUCT{ get; set; }
+        public string FIO { get; set; }
+        public string VTS{ get; set; }
+        public string CITY { get; set; }
+        public string MOBAIL { get; set; }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static SQLiteConnection connection;
+
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+
+        }
+
+        public static string FirstLetterToUpper(string str)
+        {
+            if (str.Length > 0) { return Char.ToUpper(str[0]) + str.Substring(1); }
+            return "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void turn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            searchData($"SELECT * FROM List WHERE Fio LIKE '%{edtSearch.Text.ToUpper()}%' OR Name LIKE '%{edtSearch.Text.ToUpper()}%' OR Vts LIKE '%{edtSearch.Text.ToUpper()}%'");
+
+        }
+
+        public void searchData(string patt)
+        {
+            grid.Items.Clear();
+
+            connection = new SQLiteConnection("Data Source=" + "data.db" + ";Version=3; FailIfMissing=False");
+            connection.Open();
+            
+            DataTable data = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(patt, connection);
+            adapter.Fill(data);
+            txt.Content =$"{data.Rows.Count}";
+            var pattern = @"\d-\d{3}-\d{3}-\d{2}-\d{2}";
+            Regex re = new Regex(pattern);
+
+            var i = 1;
+            var textInfo = new CultureInfo("ru-RU").TextInfo;
+
+            foreach (DataRow row in data.Rows)
+            {
+                var vID = i; //row.Field<long>("id");
+                var vMOBAIL = String.Join(" ", re.Matches(row.Field<string>("Fio")));
+                var vFIO = textInfo.ToTitleCase(row.Field<string>("Fio").Split("с.т.")[0].ToLower());
+                var vNAME = row.Field<string>("Name");
+                var vVTS = row.Field<string>("Vts");
+                var vCITY = row.Field<string>("City");
+
+
+                grid.Items.Add(new MyTable() { ID = vID.ToString(), STRUCT = vNAME, FIO = vFIO, MOBAIL = vMOBAIL, VTS = vVTS, CITY = vCITY });
+
+                i += 1;
+              
+
+            }
+            
+            connection.Close();
+
+        }
+
+        private void main_window_Loaded(object sender, RoutedEventArgs e)
+        {
+            searchData("SELECT Id, Name, Fio, Vts, City  FROM List");
+        }
+
+        private void edtSearch_KeyUp(object sender, KeyEventArgs e)
+        {
+            btnSearch_Click(null, null);
+        }
+    }
+}
