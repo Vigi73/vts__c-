12d6c7224bfd76e97aaf9aa4c2282703835b80ca
Egioya Evgeniy <vigi73@mail.ru>
2021-11-15 16:25:06 +0700

    edit tooltip

--- phone/obj/Release/net5.0-windows/App.g.cs
+++ phone/obj/Release/net5.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace phone {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace phone {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.11.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.0.0")]
         public static void Main() {
             phone.App app = new phone.App();
             app.InitializeComponent();
